1c5419798272f987ac14bbd96ff04394ddb17b24
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-24 19:52:51 -0300

    Criação do programa 'caixaEletronico' e criação de comandos para receber os valores 'nome' e 'CPF

---
+++ caixaEletronico.js
@@ -0,0 +1,22 @@
+// 1. Solicite o nome e cpf do seu cliente
+// 2. Ofereça a opção de saque ou depósito
+// 3. Pergunte um valor
+// 4. Caso a opção de saque seja a escolhida, remova o valor
+// inserido do saldo total.
+// 5. Caso a opção de depósito seja escolhida, adicione o valor
+// inserido do saldo total
+// 6. Pergunte se o seu usuário deseja continuar. (Insira 1 para
+// continuar e 2 para parar)
+// 7. Exiba o saldo total
+// 8. Exiba o maior valor inserido(independente de ser depósito ou
+// saque)
+// 9. Insira a média dos valores inseridos(independente de ser
+// depósito ou saque)
+// 10. No final mostre todos os valores inseridos pelo usuário e o
+// cpf dele
+// REGRAS:
+// 1- Não deve ser permitido sacar um valor não disponível no saldo.
+// 2- Não deve ser permitido sacar nem depositar um valor negativo.
+
+nome = prompt("Insira o seu nome")
+cpf = prompt("Insira o seu CPF")
